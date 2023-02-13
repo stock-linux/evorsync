e731c27723b87154b11615ea230aa7cf71d5de23
Skythrew <mael.guerin@outlook.fr>
2023-02-13 17:34:41 +0100

    fix: Prevent an old package from overwriting its new version

--- evorsync.sh
+++ evorsync.sh
@@ -37,14 +37,21 @@ while read line; do
                     version=$(echo $line | awk '{print $2}' | xargs)
                     release=$(echo $line | awk '{print $3}' | xargs)
                     
-                    commonlines=$(comm -12 <(sort /var/evobld/$repo/INDEX) <(sort /var/evobld/$repo/DIST))
-                    while IFS= read -r commonline ; do
-                        trimmed_line=$(echo $commonline | xargs)
+                    while read -r line; do
+                        trimmed_line =  $(echo $line | xargs)
                         if [ "$trimmed_line" = "$name $version $release" ]; then
-                            echo "Package $name is already in the repos, skipping it."
+                            echo "Package $name already in repos, skipping it."
                             continue 2
                         fi
-                    done <<< "$commonlines"
+                        pkg_name=$(echo $trimmed_line | awk '{print $1}' | xargs)
+                        pkg_version=$(echo $trimmed_line | awk '{print $2}' | xargs)
+                        pkg_release=$(echo $trimmed_line | awk '{print $3}' | xargs)
+
+                        if [ "$pkg_name" = "$name" ] && [ "$pkg_version" = "$version" ] && [ "$pkg_release" -gt "$release"]; then
+                            echo "Package $name is more up-to-date on $repo than local, skipping it."
+                            continue 2
+                        fi
+                    done  < /var/evobld/$repo/DIST
                     echo "Processing $name"
                             
                     sed -i "/$name/d" /var/evobld/$repo/DIST
