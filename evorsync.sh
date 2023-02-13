96ce95c6d237f1c8ced0bd6c4c2adb5473280f44
Skythrew <mael.guerin@outlook.fr>
2023-02-13 10:53:07 +0100

    style: Clean the code

--- evorsync.sh
+++ evorsync.sh
@@ -12,12 +12,12 @@ while read line; do
             echo "FTP Host: "
             read HOST < /dev/tty
         fi
-
+        
         if [ "$BASEDIR" = "" ]; then
             echo "FTP Packages Directory: "
             read BASEDIR < /dev/tty
         fi
-
+        
         if [ "$USER" = "" ]; then
             echo "FTP Username: "
             read USER < /dev/tty
@@ -27,7 +27,7 @@ while read line; do
             echo "FTP Password: "
             read -s PASSWORD < /dev/tty
         fi
-
+        
         if [[ $repo_path == http* ]]; then
             if [ -d /var/evobld/$repo ]; then
                 echo "Syncing --- $repo ---"
@@ -36,18 +36,18 @@ while read line; do
                     name=$(echo $line | awk '{print $1}' | xargs)
                     version=$(echo $line | awk '{print $2}' | xargs)
                     release=$(echo $line | awk '{print $3}' | xargs)
-
+                    
                     comm -12 <(sort /var/evobld/$repo/INDEX) <(sort /var/evobld/$repo/DIST) | while read commonline ; do
                         trimmed_line=$(echo $commonline | xargs)
                         if [ ! "$trimmed_line" = "$name $version $release" ]; then
                             echo "Processing $name"
-
+                            
                             sed -i "/$name/d" /var/evobld/$repo/DIST
                             echo "$name $version $release" >> /var/evobld/$repo/DIST
-
+                            
                             FILE="/var/evobld/$repo/$name-$version.evx"
                             FILENAME="$name-$version.evx"
-
+                            
                             ftp -n $HOST <<END_SCRIPT
 user $USER $PASSWORD
 cd $BASEDIR/$repo
